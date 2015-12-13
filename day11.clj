9f90ff3be05717ce959c81f17a1e6340f336d416
Stephen Lester <stephen@lester.io>
2015-12-12 16:04:40 -0800

    Day 11, parts 1 and 2

--- day11.clj
+++ day11.clj
@@ -5,7 +5,7 @@
 
 (defn next-letter [letter]
   (let [n (inc (count (take-while (partial not= letter) abcs)))]
-    (if (= n (count abcs)) \a (nth abcs n))))
+    (if (>= n (count abcs)) \a (nth abcs n))))
 
 (defn inc-password [password]
   ;; Incrementing is just like counting with numbers: xx, xy, xz, ya, yb, and so on.
@@ -14,28 +14,27 @@
   (let [pass (reverse password)]
     (loop [pos 0
            cl (next-letter (nth pass 0))
-           incpass cl]
+           incpass (list cl)]
       (if (= cl \a)
         (let [newpos (inc pos)
               newcl (next-letter (nth pass newpos))
-              newincpass (conj incpass newcl) ]
+              newincpass (conj incpass newcl)]
           (recur newpos newcl newincpass))
-        (clojure.string/join "" (reverse (str (drop (inc pos) pass) incpass)))
-        )
-      )
-    )
-  )
+        (apply conj incpass (drop (inc pos) pass))))))
 
 (defn next-password [password]
   ;; Passwords must include one increasing straight of at least three letters, like abc, bcd, cde, and so on, up to xyz. They cannot skip letters; abd doesn't count.
   ;; Passwords may not contain the letters i, o, or l, as these letters can be mistaken for other characters and are therefore confusing.
   ;; Passwords must contain at least two different, non-overlapping pairs of letters, like aa, bb, or zz.
-  password
-  )
+  (loop [n (inc-password password)]
+    (let [strn (clojure.string/join "" n)]
+      (if (or
+            (nil? (re-matches #".*(abc|bcd|cde|def|pqr|qrs|rst|stu|tuv|uvw|vwx|wxy|xyz).*" strn))
+            (nil? (re-matches #"^.*(\w)\1.*((?!\1).)\2.*$" strn)))
+        (recur (inc-password n))
+        strn))))
 
-(println abcs)
-(println (next-letter \a))
-(println (next-letter \h))
-(println (next-letter \z))
-(println (inc-password "a"))
-(println (str "Part 1: " (next-password input)))
+(def part1 (next-password input))
+(def part2 (next-password part1))
+(println (str "Part 1: " part1))
+(println (str "Part 2: " part2))
