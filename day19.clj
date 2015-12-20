c397b6bb070d445eecf0ea825d91e6cce85aaa42
Stephen Lester <stephen@lester.io>
2015-12-20 15:14:47 -0800

    Day 19, part 2

--- day19.clj
+++ day19.clj
@@ -4,30 +4,28 @@
 
 (let [input (slurp "input/19.txt")
       rules (apply merge-with into (map (fn [l] (let [[all m r] (re-matches #"(\w+) => (\w+)" l)] {m [r]}))  (clojure.string/split input #"\n")))
-      replacements (apply merge-with into (map (fn [l] (let [[all m r] (re-matches #"(\w+) => (\w+)" l)] {r m}))  (clojure.string/split input #"\n")))
+      replacements  (map (fn [l] (let [[all m r] (re-matches #"(\w+) => (\w+)" l)] [r m]))  (clojure.string/split input #"\n"))
       m "CRnCaCaCaSiRnBPTiMgArSiRnSiRnMgArSiRnCaFArTiTiBSiThFYCaFArCaCaSiThCaPBSiThSiThCaCaPTiRnPBSiThRnFArArCaCaSiThCaSiThSiRnMgArCaPTiBPRnFArSiThCaSiRnFArBCaSiRnCaPRnFArPMgYCaFArCaPTiTiTiBPBSiThCaPTiBPBSiRnFArBPBSiRnCaFArBPRnSiRnFArRnSiRnBFArCaFArCaCaCaSiThSiThCaCaPBPTiTiRnFArCaPTiBSiAlArPBCaCaCaCaCaSiRnMgArCaSiThFArThCaSiThCaSiRnCaFYCaSiRnFYFArFArCaSiRnFYFArCaSiRnBPMgArSiThPRnFArCaSiRnFArTiRnSiRnFYFArCaSiRnBFArCaSiRnTiMgArSiThCaSiThCaFArPRnFArSiRnFArTiTiTiTiBCaCaSiRnCaCaFYFArSiThCaPTiBPTiBCaSiThSiRnMgArCaF"
-      sm (split m)
-      ;; terminals (filter some? (distinct (flatten (map (fn [r] (map #(if (contains? rules %) nil %) (split (name r)))) (keys replacements)))))
-      ]
+      sm (split m)]
 
 
-  ; e => HF or NAl or OMg
-  (defn build-molecule [m]
-    (let [reps (map split (rules m))
-          ]
-      (map (fn [l] (map println l)) reps)
-      )
-    )
+  (println "Part 1:"
+           (count (distinct (filter some? (flatten (for [i (range (count sm))
+                                                                   :let [sym (nth sm i)
+                                                                         reps (or (rules sym) sym)]]
+                                                               (if (vector? reps)
+                                                                 (map #(str (clojure.string/join #"" (take i sm)) % (clojure.string/join #"" (drop (inc i) sm))) reps)
+                                                                 nil)))))))
 
-  (println (build-molecule "e"))
-
-  ;; (println "Part 1:"
-  ;;          (count (distinct (filter some? (flatten (for [i (range (count sm))
-  ;;                                                                  :let [sym (nth sm i)
-  ;;                                                                        reps (or (rules sym) sym)]]
-  ;;                                                              (if (vector? reps)
-  ;;                                                                (map #(str (clojure.string/join #"" (take i sm)) % (clojure.string/join #"" (drop (inc i) sm))) reps)
-  ;;                                                                nil)))))))
-
-
+  (println "Part 2:" (loop [li [0 m]]
+                       (let [[num-replacements final-string] li]
+                       (if (= final-string "e")
+                         num-replacements
+                         (recur (reduce (fn [[c mstr] [s t]]
+                                                (loop [new-c c
+                                                       m mstr]
+                                                  (let [new-m (clojure.string/replace-first m s t)]
+                                                    (if (= m new-m)
+                                                      [new-c new-m]
+                                                      (recur (inc new-c) new-m))))) [num-replacements final-string] replacements))))))
 )
