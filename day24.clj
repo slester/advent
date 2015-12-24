d501899810c1e2d6e1c15517ef8f6db161517e8a
Stephen Lester <stephen@lester.io>
2015-12-23 21:48:58 -0800

    Day 24 (#13), parts 1 and 2

---
+++ day24.clj
@@ -0,0 +1,15 @@
+(require 'leiningen.exec
+         '[clojure.math.combinatorics :as comb])
+
+(let [presents (map read-string (clojure.string/split (slurp "input/24.txt") #"\n"))
+      total-sum (apply + presents)]
+
+  (println "Part 1:" (apply min (map #(apply * %)
+                                     (loop [c 1]
+                                       (let [ms (filter #(= (/ total-sum 3) (apply + %)) (comb/combinations presents c))]
+                                         (if (< 0 (count ms)) ms (recur (inc c))))))))
+
+  (println "Part 2:" (apply min (map #(apply * %)
+                                     (loop [c 1]
+                                       (let [ms (filter #(= (/ total-sum 4) (apply + %)) (comb/combinations presents c))]
+                                         (if (< 0 (count ms)) ms (recur (inc c)))))))))
